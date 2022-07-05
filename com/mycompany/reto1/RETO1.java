3f9f48a2de523dd4b9d0ee69842ae2b2f0342840
jhonjairowilches <108105149+jhonjairowilches@users.noreply.github.com>
2022-07-04 23:43:28 -0500

    Add files via upload

---
+++ com/mycompany/reto1/RETO1.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.reto1;
+import Vistas.*;
+import Modelo.*;
+        
+/**
+ *
+ * @author Jhon jairo wilches
+ * Grupo 55. Reto1
+ */
+public class RETO1 {
+
+    public static void main(String[] args) {
+        // Crear instancia del Jframe Login
+        Login login = new Login ();
+        login.setVisible(true);
+        
+                
+    }
+}
